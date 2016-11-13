851c77142669ce3df20d347ac62588950ae111f7
damon bakker <damon.bakker@hotmail.com>
2016-11-13 21:01:29 +0100

    New views directories added, authorization method completed using BCrypt, feedback if user has incorrect email or password implement, viewswitcher implemented

---
+++ src/rekoffer/views/ViewSwitcher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rekoffer.views;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Damon
+ */
+public class ViewSwitcher {
+   
+    public void switchView(String viewName, Stage stage) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource(viewName));  
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
