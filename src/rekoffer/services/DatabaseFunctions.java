03bd52b519180640b47f7e33c09d2157c07089a0
Wyomi Beuker <wyomipeggy@hotmail.com>
2016-11-16 19:09:07 +0100

    added zip, country, telephone, email, colour, suitcase type and suitcase brand to the database

--- src/rekoffer/services/DatabaseFunctions.java
+++ src/rekoffer/services/DatabaseFunctions.java
@@ -11,56 +11,49 @@ import java.sql.ResultSet;
 import static rekoffer.services.Connect.getConnection;
 import static rekoffer.services.Statements.*;
 
-
 /**
  *
  * @author Damon
  */
 public class DatabaseFunctions {
-    
+
     public static java.sql.Connection con;
     public static PreparedStatement preparedStatement;
     public static ResultSet res;
-    
-    
-    public static void connect()
-    {
+
+    public static void connect() {
         con = getConnection();
-       
+
     }
-    public static void disconnect() throws SQLException
-    {
+
+    public static void disconnect() throws SQLException {
         con.close();
         res.close();
         preparedStatement.close();
     }
-    
-    public static ResultSet getUser(String userId) throws SQLException
-    {
+
+    public static ResultSet getUser(String userId) throws SQLException {
         connect();
         preparedStatement = con.prepareStatement(getUser);
         preparedStatement.setString(1, userId);
         preparedStatement.setMaxRows(1);
         res = preparedStatement.executeQuery();
 
-     
         return res;
     }
-    public static ResultSet getUserByEmail(String userEmail) throws SQLException
-    {
+
+    public static ResultSet getUserByEmail(String userEmail) throws SQLException {
         connect();
         preparedStatement = con.prepareStatement(getUserByEmail);
         preparedStatement.setString(1, userEmail);
         preparedStatement.setMaxRows(1);
         res = preparedStatement.executeQuery();
 
-     
         return res;
     }
-    
-    //data base for missing baggage
-    public static ResultSet getBaggageByLabel(String label) throws SQLException
-    {
+
+    //database for missing baggage
+    public static ResultSet getBaggageByLabel(String label) throws SQLException {
         connect();
         preparedStatement = con.prepareStatement(getBaggageByLabel);
         preparedStatement.setString(1, label);
@@ -68,8 +61,8 @@ public class DatabaseFunctions {
 
         return res;
     }
-     public static ResultSet getBaggageByName(String name) throws SQLException
-    {
+
+    public static ResultSet getBaggageByName(String name) throws SQLException {
         connect();
         preparedStatement = con.prepareStatement(getBaggageByName);
         preparedStatement.setString(1, name);
@@ -77,11 +70,74 @@ public class DatabaseFunctions {
 
         return res;
     }
-      public static ResultSet getBaggageByAddress(String adress) throws SQLException
-    {
+
+    public static ResultSet getBaggageByAddress(String adress) throws SQLException {
         connect();
         preparedStatement = con.prepareStatement(getBaggageByAddress);
         preparedStatement.setString(1, adress);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageByZip(String zip) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageByZip);
+        preparedStatement.setString(1, zip);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageByCountry(String country) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageByCountry);
+        preparedStatement.setString(1, country);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageByTelephone(String telephone) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageByTelephone);
+        preparedStatement.setString(1, telephone);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageByEmail(String email) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageByEmail);
+        preparedStatement.setString(1, email);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageByColour(String colour) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageByColour);
+        preparedStatement.setString(1, colour);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageBySuitcasetype(String suitcasetype) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageBySuitcasetype);
+        preparedStatement.setString(1, suitcasetype);
+        res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static ResultSet getBaggageBySuitcasebrand(String suitcasebrand) throws SQLException {
+        connect();
+        preparedStatement = con.prepareStatement(getBaggageBySuitcasebrand);
+        preparedStatement.setString(1, suitcasebrand);
         res = preparedStatement.executeQuery();
 
         return res;
--- src/rekoffer/services/Statements.java
+++ src/rekoffer/services/Statements.java
@@ -12,7 +12,15 @@ package rekoffer.services;
 public class Statements {
     public static String getUser = "SELECT * from users WHERE id = ?;";
     public static String getUserByEmail = "SELECT * from users WHERE email = ?;";
+    //database for missing baggage
     public static String getBaggageByLabel = "SELECT * from baggage WHERE label = ?;";
     public static String getBaggageByName = "SELECT * from baggage WHERE name = ?;";
     public static String getBaggageByAddress = "SELECT * from baggage WHERE address = ?;";
+    public static String getBaggageByZip = "SELECT * from baggage WHERE zip = ?;";
+    public static String getBaggageByCountry = "SELECT * from baggage WHERE country = ?;";
+    public static String getBaggageByTelephone = "SELECT * from baggage WHERE telephone = ?;";
+    public static String getBaggageByEmail = "SELECT * from baggage WHERE email = ?;";
+    public static String getBaggageByColour = "SELECT * from baggage WHERE colour = ?;";
+    public static String getBaggageBySuitcasetype = "SELECT * from baggage WHERE suitcasetype = ?;";
+    public static String getBaggageBySuitcasebrand = "SELECT * from baggage WHERE suitecasebrand = ?;";
 }
