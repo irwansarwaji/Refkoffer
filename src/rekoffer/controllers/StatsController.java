31647612eaeaf81c6e0c392365a4fe4dc7b0e754
Mohamed El Hallaoui <m.elhallaoui@hotmail.nl>
2016-12-05 09:21:32 +0100

    Stats scherm opnieuw gebouwd, knoppen fout geprogrammeerd, daarna weer teruggedraaid

---
+++ src/rekoffer/controllers/StatsController.java
@@ -0,0 +1,27 @@
+/**
+ * Controller voor de stats view
+ *
+ */
+package rekoffer.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mohamed El Hallaoui
+ */
+public class StatsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+    }    
+    
+}
