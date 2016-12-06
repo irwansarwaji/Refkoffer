12b6fe6396c4b8a5d3f7e2d4c568ced771032dcd
Mohamed El Hallaoui <m.elhallaoui@hotmail.nl>
2016-12-06 13:51:54 +0100

    button 'terug' on stats screen now switches to managerdashboard

--- src/rekoffer/controllers/StatsController.java
+++ src/rekoffer/controllers/StatsController.java
@@ -4,9 +4,13 @@
  */
 package rekoffer.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import rekoffer.views.ViewSwitcher;
 
 /**
  * FXML Controller class
@@ -15,13 +19,18 @@ import javafx.fxml.Initializable;
  */
 public class StatsController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private void returnDashboard(ActionEvent event) throws IOException {
+        ViewSwitcher switcher = new ViewSwitcher();
+        switcher.switchView("employee/Dashboard.fxml", event);
+
+  
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        
-    }    
-    
+       
+    }
+
 }
